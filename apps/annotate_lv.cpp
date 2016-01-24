3049defb2fe782c844e0e58f354a86d8ac70d3d2
brotherofken <brotherofken@gmail.com>
2016-01-24 20:32:49 +0300

    Better output, do nothing if one of input files is missing

--- apps/annotate_lv.cpp
+++ apps/annotate_lv.cpp
@@ -13,7 +13,12 @@
 
 
 #include <opencv2/opencv.hpp>
-#include "annotate_lv.h"
+
+inline bool ends_with(const std::string& s, const std::string& end)
+{
+	if (end.size() > s.size()) return false;
+	return std::equal(end.rbegin(), end.rend(), s.rbegin());
+}
 
 struct Slice
 {
@@ -22,6 +27,7 @@ struct Slice
 	cv::Vec3d col_dc;
 	cv::Vec3d position;
 	cv::Vec3d pixel_spacing;
+	double intercept;
 
 	cv::Mat1d rotation_matrix() const
 	{
@@ -32,10 +38,27 @@ struct Slice
 		return rm.reshape(1, 3);
 	}
 	cv::Vec3d normal() const { return row_dc.cross(col_dc); };
+
+	cv::Point2d project_point(const cv::Vec3d& p)
+	{
+		const cv::Mat1d projection = rotation_matrix().inv().t() * cv::Mat1d(p - position);
+		intercept = projection(1, 0);
+
+		return{ projection(0, 0), projection(1,0) };
+	}
 };
 
 Slice read_dcm(const std::string& filename)
 {
+	//gdcm::Reader reader;
+	//reader.SetFileName(filename.c_str());
+	//if (!reader.Read()) {
+	//	return Slice();
+	//}
+	//const gdcm::File &file = reader.GetFile();
+	//const gdcm::DataSet &ds = file.GetDataSet();
+	//std::cout << ds.GetDataElement(gdcm::PrivateTag(gdcm::Tag(0x0020, 0x1041))) << std::endl;
+
 	// Read Imageimage_position
 	gdcm::ImageReader ir;
 	ir.SetFileName(filename.c_str());
@@ -60,7 +83,8 @@ Slice read_dcm(const std::string& filename)
 				cv::Vec3d(gimage.GetDirectionCosines()),
 				cv::Vec3d(gimage.GetDirectionCosines() + 3),
 				cv::Vec3d(gimage.GetOrigin()),
-				cv::Vec3d(gimage.GetSpacing())
+				cv::Vec3d(gimage.GetSpacing()),
+				gimage.GetIntercept()
 			};
 }
 
@@ -83,12 +107,6 @@ cv::Vec3d slices_intersection(const Slice& s1, const Slice& s2, const Slice& s3)
 	return cv::Vec3d(intersection);
 }
 
-cv::Point2d point2slice(const cv::Vec3d& p, const Slice& s)
-{
-	const cv::Mat1d projection = s.rotation_matrix().inv().t() * cv::Mat1d(p - s.position);
-	return { projection(0, 0), projection(1,0) };
-}
-
 int main(int argc, char *argv[])
 {
 	if (argc < 4) {
@@ -98,27 +116,34 @@ int main(int argc, char *argv[])
 	const std::string slicecname_4ch = argv[2];
 	const std::string slicecname_sax = argv[3];
 
+	if (!ends_with(slicecname_2ch,"dcm") || !ends_with(slicecname_4ch, "dcm") || !ends_with(slicecname_sax, "dcm")) {
+		std::cerr << "skip " << slicecname_sax << std::endl;
+		return EXIT_FAILURE;
+	}
+
 	Slice slice_2ch = read_dcm(slicecname_2ch);
 	Slice slice_4ch = read_dcm(slicecname_4ch);
 	Slice slice_sax = read_dcm(slicecname_sax);
 
 	cv::Vec3d intersection = slices_intersection(slice_2ch, slice_4ch, slice_sax);
-	cv::Point2d intersection_sax = point2slice(intersection, slice_sax);
+	cv::Point2d intersection_sax = slice_sax.project_point(intersection);
 	double vmin, vmax;
 	cv::minMaxLoc(slice_sax.image, &vmin, &vmax);
 	cv::Mat1d image = (slice_sax.image - vmin) / (vmax - vmin);
 
 	std::cout
 		<< slicecname_sax << "\t"
+		<< intersection[1] << "\t" // Write Y coordinate in order to properly sort SAX slices
 		<< intersection_sax.x << "\t"
 		<< intersection_sax.y << "\t"
 		<< intersection_sax.x / slice_sax.image.rows << "\t"
 		<< intersection_sax.y / slice_sax.image.cols << std::endl;
 
-#if _DEBUG || 1
+#if _DEBUG
 	cv::circle(image, intersection_sax, 2, cv::Scalar(255));
 	cv::imshow("image", image);
 	cv::waitKey(0);
+	cv::resize(image, image, image.size() / 3);
 	cv::imwrite("1.png", 255*image);
 #endif
 
