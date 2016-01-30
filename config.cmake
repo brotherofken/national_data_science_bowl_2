c9c93b70d81f7ae408b8e2a25ba2c88ad5686c13
brotherofken <brotherofken@gmail.com>
2016-01-30 18:26:16 +0300

    Unsupported constexpr on VS2015 add CmakeLists

---
+++ config.cmake
@@ -0,0 +1,9 @@
+#set(OpenCV_DIR "")
+
+#set(BOOST_ROOT "D:/lib/boost_1_59_0/lib64-msvc-14.0" CACHE STRING "")
+set(BOOST_INCLUDEDIR "D:/lib/boost_1_59_0/" CACHE STRING "")
+set(BOOST_LIBRARYDIR "D:/lib/boost_1_59_0/lib64-msvc-14.0" CACHE STRING "")
+
+set(OpenCV_DIR "D:/lib/opencv/build" CACHE PATH "")
+
+#lib64-msvc-12.0
